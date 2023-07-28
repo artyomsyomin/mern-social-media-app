efe261a820fb9bdee65b17527e21cb95e097d05d
artyomsyomin <mr.smit333@gmail.com>
2023-07-28 17:55:06 +0700

    feat: created userWidget, fixed token error startsWith

--- client/src/scenes/navbar/Navbar.jsx
+++ client/src/scenes/navbar/Navbar.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullname = `${user.firstName} ${user.lastName}`;
-  const fullname = `Artyom Syomin`;
+  const fullname = `${user.firstName} ${user.lastName}`;
+  // const fullname = `Firstname Lastname`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
