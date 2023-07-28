7e932ed27e66a1cc7a804a64bc1c56b8bca7037c
artyomsyomin <mr.smit333@gmail.com>
2023-07-28 15:00:36 +0700

    feat: added login and register pages and logic

--- client/src/scenes/loginPage/LoginPage.jsx
+++ client/src/scenes/loginPage/LoginPage.jsx
@@ -1,6 +1,34 @@
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from './Form'
+
 const LoginPage = () => {
-    return <div>Login Page</div>;
-  };
-  
-  export default LoginPage;
-  
+  const theme = useTheme();
+
+  const alt = theme.palette.background.alt;
+
+  const isNonMobileScreen = useMediaQuery('(min-width: 1000px)');
+
+  return (
+    <Box>
+      <Box width="100%" backgroundColor={alt} p="1rem 6%" textAlign="center">
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreen ? '50%' : '93%'}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
+          Welcome to Sociopedia!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
